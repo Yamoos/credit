58da202621caa4b460f467ee7a3bf260474797d7
Yamus <yamus.ego.ya@gmail.com>
2017-01-24 02:10:07 +0200

    регистрация пользователей

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
-  root 'static_page#home'
-
-  match '/online', to: 'static_page#online', via: 'get'
-  match '/blog', to: 'static_page#blog', via: 'get'
-  match '/sity', to: 'static_page#sity', via: 'get'
-  match '/rank', to: 'static_page#rank', via: 'get'
-  match '/rank', to: 'static_page#rank', via: 'get'
+	root 'static_page#home'
+	resources :users
 
 
+	resources :sessions, only: [:new, :create, :destroy]
+	match '/signup',  to: 'users#new',            via: 'get'
+	match '/signin',  to: 'sessions#new',         via: 'get'
+	match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-  get 'static_page/calck' #не работет
+	match '/online', to: 'static_page#online', via: 'get'
+	match '/blog', to: 'static_page#blog', via: 'get'
+	match '/sity', to: 'static_page#sity', via: 'get'
+	match '/rank', to: 'static_page#rank', via: 'get'
+	match '/rank', to: 'static_page#rank', via: 'get'
+
+
+
+  	get 'static_page/calck' #не работет
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
